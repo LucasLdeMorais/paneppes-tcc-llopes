be5266aba3b842691241aae519dd7d3adfcaec23
LucasLdeMorais <lucasldemorais@gmail.com>
2023-01-04 23:16:50 -0300

    Updates

---
+++ precache-manifest.44b6e3bcfefa64e0aeeaf887cd8fc50b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "7de7e98bb8be7b2c8187",
+    "url": "/painel-visepes/static/js/0.703b6d82.chunk.js"
+  },
+  {
+    "revision": "dcfa8df1d140ac14f5f6",
+    "url": "/painel-visepes/static/js/1.06ade4c2.chunk.js"
+  },
+  {
+    "revision": "24b0c66a09174afffe84",
+    "url": "/painel-visepes/static/js/2.28037b95.chunk.js"
+  },
+  {
+    "revision": "a274abc985d0d4630940",
+    "url": "/painel-visepes/static/css/main.e09af352.chunk.css"
+  },
+  {
+    "revision": "a274abc985d0d4630940",
+    "url": "/painel-visepes/static/js/main.dc2c9cb6.chunk.js"
+  },
+  {
+    "revision": "0cb3c37d2eb6d6101ff7",
+    "url": "/painel-visepes/static/js/runtime~main.c11aafc1.js"
+  },
+  {
+    "revision": "6276f13cb9940ddb808a",
+    "url": "/painel-visepes/static/js/5.a7a35312.chunk.js"
+  },
+  {
+    "revision": "deefdb6e18aa27bc7f4b",
+    "url": "/painel-visepes/static/css/6.3fc62208.chunk.css"
+  },
+  {
+    "revision": "deefdb6e18aa27bc7f4b",
+    "url": "/painel-visepes/static/js/6.05a44217.chunk.js"
+  },
+  {
+    "revision": "894852adfc9b1a5b5be6",
+    "url": "/painel-visepes/static/css/7.9616b43e.chunk.css"
+  },
+  {
+    "revision": "894852adfc9b1a5b5be6",
+    "url": "/painel-visepes/static/js/7.02c92df4.chunk.js"
+  },
+  {
+    "revision": "09b37f6d6633c3440923",
+    "url": "/painel-visepes/static/css/8.b7818b40.chunk.css"
+  },
+  {
+    "revision": "09b37f6d6633c3440923",
+    "url": "/painel-visepes/static/js/8.d195015f.chunk.js"
+  },
+  {
+    "revision": "c68b01eaedc0467b4d31",
+    "url": "/painel-visepes/static/css/9.669e2310.chunk.css"
+  },
+  {
+    "revision": "c68b01eaedc0467b4d31",
+    "url": "/painel-visepes/static/js/9.2f00ab17.chunk.js"
+  },
+  {
+    "revision": "203f0b26c08ac8cfae9b",
+    "url": "/painel-visepes/static/js/10.dc1126d9.chunk.js"
+  },
+  {
+    "revision": "2b92b59b18b8506bea49",
+    "url": "/painel-visepes/static/css/11.85a58f3c.chunk.css"
+  },
+  {
+    "revision": "2b92b59b18b8506bea49",
+    "url": "/painel-visepes/static/js/11.968778af.chunk.js"
+  },
+  {
+    "revision": "dfa525b298f120193fc6",
+    "url": "/painel-visepes/static/css/12.e19e7053.chunk.css"
+  },
+  {
+    "revision": "dfa525b298f120193fc6",
+    "url": "/painel-visepes/static/js/12.e6a1cba5.chunk.js"
+  },
+  {
+    "revision": "7ad20ca2c7f643068d267b3a5d41d581",
+    "url": "/painel-visepes/index.html"
+  }
+];
