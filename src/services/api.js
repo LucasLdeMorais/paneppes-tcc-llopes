c0f61927ced0e5fa7dd6a68e1f82783c32c63f9f
LucasLdeMorais <lucasldemorais@gmail.com>
2023-01-14 18:36:05 -0300

    ajustes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'https://6xih7zuq2l.execute-api.sa-east-1.amazonaws.com/dev/'
 })
 
 export default api;
